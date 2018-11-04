ca9068fbcf275b0e5e8be95c06e562cf4495116a
Tom Metz <tomas@metzovi.net>
2018-11-04 14:39:16 +0100

    feature (http): add helper for simpler production of  HTTP responses

---
+++ src/common/ApiResponse.js
@@ -0,0 +1,36 @@
+'use strict'
+
+function http200(req, res, payload) {
+  res
+    .type('application/json')
+    .status(200)
+    .send(payload)
+}
+
+function http400(req, res) {
+  res
+    .type('application/json')
+    .status(400)
+    .send()
+}
+
+function http405(req, res) {
+  res
+    .type('application/json')
+    .sendStatus(405)
+    .send()
+}
+
+function http500(req, res) {
+  res
+    .type('application/json')
+    .sendStatus(500)
+    .send()
+}
+
+module.exports = {
+  http200: http200,
+  http400: http400,
+  http405: http405,
+  http500: http500,
+}
