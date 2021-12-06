18d3d6f84520b7fb20b9bdf662c430278f15fe2d
Tomas Metz <tomas_metz@mckinsey.com>
2021-12-06 10:37:18 +0100

    chore (application): update CSV export script about new Family attributes

--- scripts/json2csv.js
+++ scripts/json2csv.js
@@ -1,12 +1,17 @@
 #!/usr/local/bin/node
 const json2csv = require('json2csv')
 
-const familyList = require('./final-family.json')
+const centerList = require('../__data__/center.json')
+const familyList = require('./family_final_2021.json')
+const placeList = require('../__data__/place.json')
+
+const centerDict = _centerListToDict(centerList)
+const placeDict = _placeListToDict(placeList)
 
 const data = []
 
-familyList.forEach(family => {
-  family.children.forEach(child => {
+familyList.forEach((family) => {
+  family.children.forEach((child) => {
     data.push({
       familyId: family.id,
       name: child.name,
@@ -14,7 +19,11 @@ familyList.forEach(family => {
       gender: child.gender,
       donor: family.contact.name,
       email: family.contact.email,
-      place: codeToPlaceName(family.gatheringPlaces[0]),
+      chosenOriginCenter:
+        centerDict[family.chosenCenterId]?.name ||
+        'MISSING',
+      originCenter: centerDict[family.centerId].name,
+      originPlace: placeDict[family.placeId].name,
     })
   })
 })
@@ -26,7 +35,9 @@ const fields = [
   'gender',
   'donor',
   'email',
-  'place',
+  'chosenOriginCenter',
+  'originCenter',
+  'originPlace',
 ]
 
 const opts = { fields }
@@ -37,7 +48,9 @@ console.log(csv)
 
 //{
 //id: '25adaa83-3604-48a2-ab93-1a7215f0db41',
+//centerId: 'c0002',
 //free: false,
+//chosenCenterId: 'c0003' -- !! might be missing !!
 //children: [
 //{
 //name: 'Veronika',
@@ -46,21 +59,22 @@ console.log(csv)
 //specifics: 'Koníčky: tancování, zpěv, plavání. Sliz, panenky, kreslící potřeby, oblečení vel. L.'
 //}
 //],
-//gatheringPlaces: [ 1, 0 ],
+//placeId: 'p0001',
 //contact: { name: 'Tereza Škavronková', email: 'Terka8888@seznam.cz' }
 //}
 
-function codeToPlaceName(code) {
-  switch (code) {
-    case 0:
-      return 'Rodinné centrum Letná'
-    case 1:
-      return 'Dobrá rodina o.p.s.'
-    case 2:
-      return 'Barevný svět dětí'
-    case 3:
-      return 'Dětský domov v Přestavlkách'
-    default:
-      ''
-  }
+function _centerListToDict(centerList) {
+  return centerList.reduce((acc, center) => {
+    const centerId = center.id
+    acc[centerId] = center
+    return acc
+  }, {})
+}
+
+function _placeListToDict(placeList) {
+  return placeList.reduce((acc, place) => {
+    const placeId = place.id
+    acc[placeId] = place
+    return acc
+  }, {})
 }
